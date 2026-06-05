a6e3476316cebb129c51417c2d75ed4bf3bb012b
CattacoCattaco <cattacoiscool@gmail.com>
2026-06-05 13:27:00 -0700

    Change Tile Map to Use Set Tile
    Now the Tile Map creates all of the tiles at the start and uses a set tile function rather than the previous create tile function to display tiles. Also, added empty and apple tiles

--- tile_map/snake_tile_map.gd
+++ tile_map/snake_tile_map.gd
@@ -5,21 +5,42 @@ extends Node2D
 @export var light_palette: Texture2D
 @export var dark_palette: Texture2D
 
+@export var board_size := Vector2i(20, 20)
+
 var snake: Array[Vector2i]
-var tiles: Dictionary[Vector2i, Tile]
+var tiles: Array[Array]
 
 func _ready() -> void:
+	var offset := Vector2i(320, 180) - (board_size * 8)
+	for y in range(board_size.y):
+		tiles.append([])
+		for x in range(board_size.x):
+			var tile := Tile.new()
+			add_child(tile)
+			
+			var pos := Vector2i(x, y)
+			tile.position = pos * 16 + offset
+			tiles[y].append(tile)
+			
+			tile.tile_sprites = sprite_sheet
+			tile.color_palette = light_palette
+			tile.sprite_coords = Tile.EMPTY
+			tile.flip_h = false
+			tile.flip_v = false
+	
 	snake = [Vector2i(1, 1), Vector2i(2, 1), Vector2i(3, 1), Vector2i(3, 2), Vector2i(4, 2),
 			Vector2i(4, 3), Vector2i(4, 4), Vector2i(5, 4)]
 	draw_snake()
+	
+	set_tile(Vector2i(19, 19), Tile.APPLE, true, false, false)
 
 
 func draw_snake() -> void:
 	var facing: Vector2i = snake[0] - snake[1]
-	var head_sprite_coords := Vector2i(3, 0 if facing.y == 0 else 1)
+	var head_sprite_coords := Vector2i(Tile.HEAD_COLUMN, Tile.get_hv_row(facing.y == 0))
 	var head_flip_h: bool = facing.x == 1
 	var head_flip_v: bool = facing.y == 1
-	create_tile(snake[0], head_sprite_coords, true, head_flip_h, head_flip_v)
+	set_tile(snake[0], head_sprite_coords, true, head_flip_h, head_flip_v)
 	
 	var is_light: bool = false
 	for i in range(1, len(snake) - 1):
@@ -29,40 +50,40 @@ func draw_snake() -> void:
 		var sprite_coords: Vector2i
 		
 		if coming_from == -going_to:
-			sprite_coords = Vector2i(2, 0 if coming_from.y == 0 else 1)
+			sprite_coords = Vector2i(Tile.STRAIGHT_COLUMN, Tile.get_hv_row(coming_from.y == 0))
 		else:
 			var h_dir: Vector2i = coming_from if coming_from.y == 0 else going_to
 			var v_dir: Vector2i = coming_from if coming_from.x == 0 else going_to
 			
 			match [h_dir.x, v_dir.y]:
 				[1, 1]:
-					sprite_coords = Vector2i(0, 0)
+					sprite_coords = Tile.CORNER_BOTTOM_RIGHT
 				[1, -1]:
-					sprite_coords = Vector2i(0, 1)
+					sprite_coords = Tile.CORNER_TOP_RIGHT
 				[-1, 1]:
-					sprite_coords = Vector2i(1, 0)
+					sprite_coords = Tile.CORNER_BOTTOM_LEFT
 				[-1, -1]:
-					sprite_coords = Vector2i(1, 1)
+					sprite_coords = Tile.CORNER_TOP_LEFT
 		
-		create_tile(snake[i], sprite_coords, is_light, false, false)
+		set_tile(snake[i], sprite_coords, is_light, false, false)
 		is_light = !is_light
 	
 	var tail_dir: Vector2i = snake[-1] - snake[-2]
-	var tail_sprite_coords := Vector2i(4, 0 if tail_dir.y == 0 else 1)
+	var tail_sprite_coords := Vector2i(Tile.TAIL_COLUMN, Tile.get_hv_row(tail_dir.y == 0))
 	var tail_flip_h: bool = tail_dir.x == 1
 	var tail_flip_v: bool = tail_dir.y == 1
-	create_tile(snake[-1], tail_sprite_coords, is_light, tail_flip_h, tail_flip_v)
+	set_tile(snake[-1], tail_sprite_coords, is_light, tail_flip_h, tail_flip_v)
 
 
-func create_tile(pos: Vector2i, sprite_coords: Vector2i, is_light: bool, flip_h: bool, flip_v: bool
+func set_tile(pos: Vector2i, sprite_coords: Vector2i, is_light: bool, flip_h: bool, flip_v: bool
 		) -> void:
-	var tile := Tile.new()
-	add_child(tile)
+	var tile := get_tile(pos)
 	tile.tile_sprites = sprite_sheet
 	tile.color_palette = light_palette if is_light else dark_palette
 	tile.sprite_coords = sprite_coords
-	tile.position = pos * 16
 	tile.flip_h = flip_h
 	tile.flip_v = flip_v
-	
-	tiles[pos] = tile
+
+
+func get_tile(pos: Vector2i) -> Tile:
+	return tiles[pos.y][pos.x]
--- tile_map/tile/tile.gd
+++ tile_map/tile/tile.gd
@@ -2,6 +2,21 @@
 class_name Tile
 extends Sprite2D
 
+const CORNER_BOTTOM_RIGHT := Vector2i(0, 0)
+const CORNER_TOP_RIGHT := Vector2i(0, 1)
+const CORNER_BOTTOM_LEFT := Vector2i(1, 0)
+const CORNER_TOP_LEFT := Vector2i(1, 1)
+
+const STRAIGHT_COLUMN: int = 2
+const HEAD_COLUMN: int = 3
+const TAIL_COLUMN: int = 4
+const DEAD_COLUMN: int = 5
+const HORZONTAL_ROW: int = 0
+const VERTICAL_ROW: int = 1
+
+const APPLE := Vector2i(6, 0)
+const EMPTY := Vector2i(6, 1)
+
 @export var color_palette: Texture2D:
 	set(value):
 		if not material or material is not ShaderMaterial:
@@ -23,6 +38,11 @@ extends Sprite2D
 		texture = atlas
 
 var tile_sprites: Texture2D
+
+# Returns HORZONTAL_ROW if is_horizontal
+# [br]Otherwise, returns VERTICAL_ROW
+static func get_hv_row(is_horizontal: bool) -> int:
+	return HORZONTAL_ROW if is_horizontal else VERTICAL_ROW
 
 
 func _ready() -> void:
